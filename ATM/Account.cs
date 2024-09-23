576f716d3e4dcc42a5078325a26e83532578da96
JordainCampbell <60863792+Jordain18@users.noreply.github.com>
2024-09-22 20:10:27 -0500

    Add project files.

---
+++ ATM/Account.cs
@@ -0,0 +1,89 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Main Program 
+
+//  ATM Interface Menu 
+
+
+
+//class goes here 
+
+public class Account
+{
+    private double _accountBalance;
+
+    public Account(double initialBalance)
+    {
+        if (initialBalance >= 1000.0)
+        {
+            _accountBalance = initialBalance;
+        }
+
+        else if (initialBalance! >= 1000.0)
+        {
+            initialBalance = 0;
+            Console.WriteLine("Initial balance was invalid");
+
+        }
+
+        else
+        {
+            Console.WriteLine("Incorrect Input !");
+        }
+    }
+    // Method
+
+   public void PrintATMMenu()
+    {
+        Console.WriteLine("--- Welcome To The XYZ ATM ---");
+        Console.WriteLine();
+        Console.WriteLine("Select '1' - To  Check Account Balance");
+        Console.WriteLine("Select '2' - To  Credit Account");
+        Console.WriteLine("Select '3' -   To Debit Account");
+        Console.WriteLine("Select '4' - To  Exit The ATM ");
+
+    }
+    public void Credit(double amount)
+    {
+        if (amount > 0)
+        {
+            _accountBalance += amount;
+        }
+        else if (amount < 0)
+        {
+            Console.WriteLine("Cannot Accepted Negative Value");
+        }
+
+
+    }
+    public void Debit(double amount)
+    {
+
+        if ( amount <= 1000)
+        {
+            _accountBalance -= amount;
+
+        }
+        else if (amount > 1000)
+        {
+
+            Console.WriteLine("Debit amount exceeded account balance");
+        }
+
+
+
+    }
+
+      public double GetBalance()
+    {
+        return _accountBalance;
+    }
+}
+
+
+//int choice = 0;
+
+//switch (choice)
+//{
+//    case 1:
+//}
---
+++ ATM/Program.cs
@@ -0,0 +1,73 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Main Program 
+
+using System.Diagnostics.Tracing;
+using System.Xml.Serialization;
+
+int select = 0;
+
+Console.WriteLine("Please enter the Initial Value , Mininum a Thousand dollars ($1,000.00)");
+double balance = Convert.ToDouble(Console.ReadLine());
+if ( balance < 1000)
+{
+
+    Console.WriteLine("  Only going accept Mininum a Thousand dollars ($1,000.00)");
+    
+
+}
+else
+{
+    Account account = new Account(balance);
+    do
+    {
+        account.PrintATMMenu();
+        select = Convert.ToInt32(Console.ReadLine());
+
+        if (select == 1)
+        {
+            balance = account.GetBalance();
+            Console.WriteLine($" Your balance is {account.GetBalance()}");
+
+        }
+        else if (select == 2)
+        {
+            Console.WriteLine("Please enter the  Amount you  want take from the credit account");
+            double creditamount = Convert.ToDouble(Console.ReadLine());
+
+            account.Credit(creditamount);
+
+            Console.WriteLine($" Your New Balance is {account.GetBalance()}");
+        }
+        else if (select == 3)
+        {
+            Console.WriteLine("Please enter the  Amount you  want take from the credit account");
+            double debitamount = Convert.ToDouble(Console.ReadLine());
+
+            account.Debit(debitamount);
+
+            Console.WriteLine($" Your New Balance is {account.GetBalance()}");
+
+
+        }
+        else if (select == 4)
+        {
+            Console.Write(" Thank You for using  XYZ ATM  have an Nice Day");
+            break;
+        }
+
+        else
+        {
+            Console.WriteLine("Input Incorrect");
+        }
+    } while (select != 4);
+
+}
+
+
+//int choice = 0;
+
+//switch (choice)
+//{
+//    case 1:
+//}
